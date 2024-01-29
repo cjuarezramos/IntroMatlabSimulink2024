ae6003ebace99a05b1d05dd684af1c1d2d441e7d
cjuar <cjuar@DEI_CJ>
2024-01-29 09:17:36 -0600

    uso de input y fprintf

---
+++ CódigosPresentacion/ej3_inputfprintf.m
@@ -0,0 +1,17 @@
+% Introducción de valores numéricos:
+a = input('Ingrese un valor númerico: ');
+
+% Introducción de valores tipo caracter:
+b = input('Ingrese una oración: ','s');
+
+
+% uso de fprintf
+% s para datos tipo caracter
+% f para mostrar número forma decimal
+fprintf('Su oración es: %s y su número es %f\n',b,a) 
+% g muestra números el iliminando ceros no significativos
+fprintf('Su oración es: %s y su número es %g\n',b,a)
+% 4.2f formato decimal, 4 espacios 2 decimales.
+fprintf('Su oración es: %s y su número es %4.2f\n',b,a)
+% e notación científica.
+fprintf('Su oración es: %s y su número es %e\n',b,a)
