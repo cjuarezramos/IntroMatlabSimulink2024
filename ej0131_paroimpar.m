e245459f56a3b8121bb9eb353c5578ca029f0dc3
Carlos Juarez <cjuarez@uca.edu.sv>
2024-02-01 11:11:59 -0600

    ejemplos resueltos en el aula

---
+++ ej0131_paroimpar.m
@@ -0,0 +1,13 @@
+% if doble
+% Numero par o impar
+% Entradas:
+n = input('Ingrese numero entero: ');
+
+% division
+div = n/2;
+if div == floor(div)
+    disp('Numero par')
+else
+    disp('Numero impar')
+end
+disp('Fin del programa')
